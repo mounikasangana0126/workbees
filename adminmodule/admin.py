dfa649a4a3052f344f70efc914468427b10caf8a
Mounika Sangana <sssmounika5@gmail.com>
2024-09-03 00:02:09 +0530

    created models

--- adminmodule/admin.py
+++ adminmodule/admin.py
@@ -1,6 +1,23 @@
 from django.contrib import admin
-from adminmodule.models.department_model import DepartmentModel
+
+from adminmodule.models.designation_model import DesignationModel
+from adminmodule.models.department_model import DepartmentModel,ParentModel
+from adminmodule.models.user_model import User
+from adminmodule.models.time_entry_model import TimeEntry
+from adminmodule.models.task_model import Task
+from adminmodule.models.break_entry_model import BreakEntry
+from adminmodule.models.employee_model import Employees
+from adminmodule.models.shift_timings_model import WorkShiftsModel,UserShiftTimingsModel
 
 # Register your models here.
 
 admin.site.register(DepartmentModel)
+admin.site.register(ParentModel)
+admin.site.register(DesignationModel)
+admin.site.register(User)
+admin.site.register(TimeEntry)
+admin.site.register(Task)
+admin.site.register(BreakEntry)
+admin.site.register(Employees)
+admin.site.register(WorkShiftsModel)
+admin.site.register(UserShiftTimingsModel)
--- adminmodule/models/designation_model.py
+++ adminmodule/models/designation_model.py
@@ -1,8 +1,6 @@
 from django.db import models
-from adminmodule.models.department_model import DepartmentModel
 
 class DesignationModel(models.Model):
-    department= models.ForeignKey(DepartmentModel,on_delete=models.SET_NULL,null=True)
     designation_name = models.CharField(max_length=255,unique=True)
     designation_is_active = models.BooleanField(default=True)
 
