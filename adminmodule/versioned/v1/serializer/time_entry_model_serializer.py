4ddfbc78b7ddcdf077a51c374f8d9081fe54a761
unknown <gandepallisatish930@gmail.com>
2024-09-04 12:10:34 +0530

    updated api

--- adminmodule/versioned/v1/serializer/time_entry_model_serializer.py
+++
@@ -1,8 +0,0 @@
-
-from rest_framework import serializers
-from adminmodule.models.time_entry_model import TimeEntry
-
-class TimeEntrySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = TimeEntry
-        fields = '__all__'
--- adminmodule/versioned/v1/serializer/user_model_serializer.py
+++
@@ -1,6 +0,0 @@
-from rest_framework import serializers
-from adminmodule.models.user_model import User
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = '__all__'
