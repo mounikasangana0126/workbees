45c05157da48124b11bd9173f0dbf611a91d607e
unknown <gandepallisatish930@gmail.com>
2024-09-04 12:12:43 +0530

    updated api

--- adminmodule/urls.py
+++ adminmodule/urls.py
@@ -1,13 +1,10 @@
 
 from django.urls import path,include
 from adminmodule.versioned.v1.api.department_api import DepartmentGetAPI
-from adminmodule.versioned.v1.api.time_entry_model_api import TimeGetAPI
-from adminmodule.versioned.v1.api.user_model_api import UserGetAPI
-from . import views 
+
 
 urlpatterns = [
-   path('authentiation/',views.home),
    path('department/',DepartmentGetAPI.as_view()),
-   path('time_entry/',TimeGetAPI.as_view()),
-   path('user/',UserGetAPI.as_view()),
+   #path("satish")
+   
 ]
