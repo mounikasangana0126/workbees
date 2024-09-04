4ec39d7cca174242f45ebd727caddc427de59d5a
unknown <gandepallisatish930@gmail.com>
2024-09-04 11:52:39 +0530

    updated api

--- adminmodule/views.py
+++ adminmodule/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from adminmodule.models.authentication import AuthenticationModel
+
+def home(request):
+    return render(request,'index.html',{'message':'satish here'})
+
+def authentication (request,email,password):
+    queryset=AuthenticationModel.objects.get(email=email)
+    if queryset.password==password:
+        return render(request,'login.html',{'message':'successfully logined'})
+    return render(request,'login.html',{'message':"you entred wrong email or password"})
+
+      
 
 # Create your views here.
+
